a7098d26a27a421d6a22ba62c9507be6c4135a67
Amit Bhatt <87167386+Amitbhatt72480@users.noreply.github.com>
2023-03-09 20:52:50 +0530

    Add files via upload

---
+++ src/Requests.js
@@ -0,0 +1,11 @@
+const key = 'c7c7a846b9a72f55d3f713fb7f6435a1';
+
+const requests = {
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+  };
+
+export default requests;
